482904ff7030a626320cbf3cb276246472f42e58
Samuel Robertson <samuel.robertson93@gmail.com>
2017-02-12 20:41:14 -0800

    cleaned up some warnrnings, updated the reademe with more information for development and usage

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # JSON Data Store
-#### What is this?
+
+
+## What is this?
+---
 The JSON Data Store is a basic utility that is based on [Firebase](www.google.firebase.com)
 that allows for the quick creation of an endpoint to store JSON. It is designed to allow the load to be placed onto the client for data requests and remove the need for storage of data on wordpress or static sites. The reason we use Firebase over something like S3 is that it the JSON Data Store application allows for live or quick updates to the data.
 
-**All data is readable by the public so do not store sensetive information in the JSON Data Store.**
+Writing to the database is restricted to users based on the `@revolutionmessaging` domain in firebase.
+The API key is public knowledge as most firebase apps allow reading and writing. However in our use case we would only 
+like our developers to be able to write to the real time db. Hence the setup of these rules.
 
-#### How do I use it? 
+**All data is readable by the public so do not store sensitive information in the JSON Data Store.**
+
+Data is stored at endpoints of a JSON store name.
+
+```
+https://somefirebasedatabase-firebaseio.com/:data_store_name.json
+```
+This then returns the JSON data. 
+
+**Each name must be unique.**
+
+## How do I use it? 
+---
 First navigate to Dashboard and then Sign In with your RevMSG account. If you do not do this you will redirected back to this page. Once Signed in you will see a populated table with example data stores. A data store name is labled by **name** and is unique. If a store is created with the exact same name the data will be overwritten. These **names** are Firebase child keys and also the endpoints for the JSON data. In the table the **URL** is the REST endpoint firebase creates for your data. This is designed to be used as only a GET request but other methods are supported. Check out the firebase API docs [here](https://firebase.google.com/docs/reference/rest/database/) for those methods.
 
 To create a new JSON Store 
@@ -16,8 +33,9 @@ To create a new JSON Store
 
 You will then be taken back to the table and can view the endpoint and call your JSON externally. 
 
-#### Whats the code ?
 
+## Whats the code ?
+---
 Anyone has access to this data so we do not need to authenticate or worry about headers. A simple AJAX request will work for getting the data we need. 
 
 An example request using jQuery to get out data looks like.
@@ -33,3 +51,32 @@ $.get('https://bmgf-datastore.firebaseio.com/example_data.json')
 ```
 
 This will then return our JSON object ready to use for later in the process!
+
+## Development 
+---
+
+Clone down this project to get started, then run
+
+```
+npm install
+
+```
+and firebase init. If you do not have the firebase cli install with 
+
+```
+npm install -g firebase-tools
+```
+
+
+To start the dev server run
+```
+npm start
+```
+to build for production and before running firebase deploy make sure to run build
+```
+npm run build
+```
+
+To configure this application to your own firebase instance,
+look under `config/constants` and enter your own `apiKey`,  `authDomain`
+and `databaseURL` from your firebase application.
--- src/components/protected/ResourcesTable.js
+++ src/components/protected/ResourcesTable.js
@@ -1,84 +1,102 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router'
-import firebase from 'firebase'
+import React, { Component } from "react";
+import { Link } from "react-router";
+import firebase from "firebase";
 
 export default class ResourcesTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dataStores: []
-    }
+    };
   }
 
   componentDidMount() {
-    this.getData()
+    this.getData();
   }
 
-  getData(){
+  getData() {
     let headers = new Headers();
-    headers.append("Content-Type", "application/json")
-    fetch('https://bmgf-datastore.firebaseio.com/.json?shallow=true', { headers: headers, method: 'get' })
-      .then((resp) => {
-        return resp.json()
-      }).then((data) => {
-        let dataStores = []
-        for (const key in data) {
-          dataStores.push({ url: `https://bmgf-datastore.firebaseio.com/${key}.json`, db_name: key })
+    headers.append("Content-Type", "application/json");
+    fetch("https://bmgf-datastore.firebaseio.com/.json?shallow=true", {
+      headers: headers,
+      method: "get"
+    })
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        let dataStores = [];
+        if (data) {
+          for (const key in data) {
+            dataStores.push({
+              url: `https://bmgf-datastore.firebaseio.com/${key}.json`,
+              db_name: key
+            });
+          }
         }
-        this.setState({ dataStores })
-      }).catch((err) => {
-        console.error(err)
+        this.setState({ dataStores });
+      })
+      .catch(err => {
+        console.error(err);
       });
   }
 
-  deleteDatabase(db_name){
-    if (window.confirm("Do you really want delete this data store?")) { 
-      let db_ref = firebase.database().ref(db_name)
-      db_ref.remove()
-      this.getData()
+  deleteDatabase(db_name) {
+    if (window.confirm("Do you really want delete this data store?")) {
+      let db_ref = firebase.database().ref(db_name);
+      db_ref.remove();
+      this.getData();
     }
   }
 
-  showEditor(db_name){
-    this.props.showEditor(db_name)
+  showEditor(db_name) {
+    this.props.showEditor(db_name);
   }
 
   render() {
-
-  	let databases = this.state.dataStores || []
-  	let database_table_rows = databases.map((database) => {
-  		return (
-  			<tr key={database.db_name}>
-  				<td>{database.db_name}</td>
-  				<td><Link target="_blank" to={database.url}>{database.url}</Link></td>
-          <td><a href="#" onClick={this.showEditor.bind(this, database.db_name)}>Edit</a></td>
-  				<td><a href="#" onClick={this.deleteDatabase.bind(this, database.db_name)}>
-            <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-          </a></td>
-  			</tr>
-  		)
-  	})
+    let databases = this.state.dataStores || [];
+    let database_table_rows = databases.map(database => {
+      return (
+        <tr key={database.db_name}>
+          <td>{database.db_name}</td>
+          <td><Link target="_blank" to={database.url}>{database.url}</Link></td>
+          <td>
+            <a href="#" onClick={this.showEditor.bind(this, database.db_name)}>
+              Edit
+            </a>
+          </td>
+          <td>
+            <a
+              href="#"
+              onClick={this.deleteDatabase.bind(this, database.db_name)}
+            >
+              <span className="glyphicon glyphicon-trash" aria-hidden="true" />
+            </a>
+          </td>
+        </tr>
+      );
+    });
 
     return (
       <div>
-			<br/>
-			<div className="panel panel-default">
-			  <div className="panel-heading">Existing Resources</div>
-					<table className="table">
-				    <thead>
-			        <tr>
-			          <th>DB Name</th>
-			          <th>URL</th>
+        <br />
+        <div className="panel panel-default">
+          <div className="panel-heading">Existing Resources</div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>DB Name</th>
+                <th>URL</th>
                 <th>Edit</th>
-			          <th>Delete</th>
-			         </tr>
-					    </thead>
-					    <tbody>
-					    	{database_table_rows}
-					    </tbody>
-					</table>
-				</div>
-			</div>
-    )
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {database_table_rows}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
